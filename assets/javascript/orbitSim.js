bdebfe404c8b740fe031fedd54d8f1f6a3c2f778
Erik Alfvin <erikalfvin@gmail.com>
2020-01-04 07:45:59 -0600

    Added max altitude and min altitude calculations

--- assets/javascript/orbitSim.js
+++ assets/javascript/orbitSim.js
@@ -1,9 +1,17 @@
 var runOrbit;
 
+var thrust = 1;
+var ra;
+var rb;
+
 function orbitSimulation(){
-    document.getElementById("Hours").style.visibility = "visible";
-    //document.getElementById("Period").style.visibility = "visible";
+    document.getElementById("HoursMobile").innerHTML = "Elapsed Time = 0 Hours";
+    document.getElementById("AltMaxMobile").innerHTML = "Max Altitude = 0 km";
+    document.getElementById("AltMinMobile").innerHTML = "Min Altitude = 0 km";
+
     document.getElementById("Hours").innerHTML = "Elapsed Time = 0 Hours";
+    document.getElementById("AltMax").innerHTML = "Max Altitude = 0 km";
+    document.getElementById("AltMin").innerHTML = "Min Altitude = 0 km";
 
     const earth = document.getElementById("Earth");
     const sat = document.getElementById("Sat");
@@ -37,9 +45,11 @@ function orbitSimulation(){
     var vSat = [0.0, -1 * vInitial];
 
     var xDir = -1;
-    var ydir = 1;
+    var yDir = 1;
 
     var r;
+    ra = rInitial;
+    rb = rInitial;
 
     var theta;
     var accSat;
@@ -53,25 +63,38 @@ function orbitSimulation(){
     function run() {
         r = Math.sqrt(Math.pow(p1[0] - satPos[0], 2) + Math.pow(p1[1] - satPos[1], 2));
 
+        if (r > ra) {
+            ra = r;
+        }
+
+        if (r < rb) {
+            rb = r;
+        }
+
+        document.getElementById("AltMaxMobile").innerHTML = "Max Altitude = " + ((ra - 6.378 * Math.pow(10, 6))/1000).toFixed(0) + " km";
+        document.getElementById("AltMinMobile").innerHTML = "Min Altitude = " + ((rb - 6.378 * Math.pow(10, 6))/1000).toFixed(0) + " km";
+        document.getElementById("AltMax").innerHTML = "Max Altitude = " + ((ra - 6.378 * Math.pow(10, 6))/1000).toFixed(0) + " km";
+        document.getElementById("AltMin").innerHTML = "Min Altitude = " + ((rb - 6.378 * Math.pow(10, 6))/1000).toFixed(0) + " km";
+
         //Find new rhat vector (unit vector that points from Sat to Earth):
-        if (p1[0] < satPos[0]){
+        if (p1[0] < satPos[0] && xDir != -1){
             xDir = -1;
         }
-        else {
+        else if (p1[0] > satPos[0] && xDir != 1) {
             xDir = 1;
         }
 
-        if (p1[1] < satPos[1]) {
-            ydir = -1;
+        if (p1[1] < satPos[1] && yDir != -1) {
+            yDir = -1;
         }
-        else {
-            ydir = 1;
+        else if (p1[1] > satPos[1] && yDir != 1) {
+            yDir = 1;
         }
 
         if (p1[1] != satPos[1]) {
             theta = Math.atan(Math.abs((p1[0] - satPos[0]) / (p1[1] - satPos[1])));
             rhat[0] = Math.sin(theta) * xDir;
-            rhat[1] = Math.cos(theta) * ydir;
+            rhat[1] = Math.cos(theta) * yDir;
         }
         else {
             rhat[0] = 1 * xDir;
@@ -80,8 +103,8 @@ function orbitSimulation(){
 
         //Calculate new velocity of Satilite ten seconds later:
         accSat = 398589405759999.94 / Math.pow(r, 2); //a = GM/r^2 - assumes earth mass of 5.9722E24 kg
-        vSat[0] = vSat[0] + 10 * accSat * c * rhat[0];
-        vSat[1] = vSat[1] + 10 * accSat * c * rhat[1];
+        vSat[0] = (vSat[0] + 10 * accSat * c * rhat[0]) * thrust;
+        vSat[1] = (vSat[1] + 10 * accSat * c * rhat[1]) * thrust;
 
         //Calculate new position of satilite:
         satPos[0] = satPos[0] + 10 * vSat[0];
@@ -90,12 +113,22 @@ function orbitSimulation(){
         sat.style.left = (satPos[0] / boxWidthMeters) * 100 - 0.75 + "%";
         sat.style.top = (satPos[1] / boxWidthMeters) * 100 - 0.75 + "%";
 
+        if (thrust != 1) {
+            thrust = 1;
+
+            r = Math.sqrt(Math.pow(p1[0] - satPos[0], 2) + Math.pow(p1[1] - satPos[1], 2));
+            ra = r;
+            rb = r;
+        }
+
         if (c === 0.5) {
             c = 1;
         }
 
         iter++;
+
         if (iter % 360 === 0) {
+            document.getElementById("HoursMobile").innerHTML = "Elapsed Time = " + iter / 360 + " Hours";
             document.getElementById("Hours").innerHTML = "Elapsed Time = " + iter / 360 + " Hours";
         }
 
@@ -104,6 +137,14 @@ function orbitSimulation(){
             alert("Satellite crashed!");
         }
     }
+}
+
+function thrustUp() {
+    thrust = 1.05;
+}
+
+function thrustDown() {
+    thrust = 0.95;
 }
 
 function stopSimulation() {
